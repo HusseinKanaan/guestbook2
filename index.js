85e031e36711ae5095847a3dd553d01e4c8ffc5e
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-10-04 23:52:57 +0200

    new commit

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 import express from 'express'
+import { persons } from './data.js'
 
-const PORT = process.env.PORT || 9999;
-
-// your code
-
+const PORT = 9898;
 const app = express()
 
 
 
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({ extension: true }))
+
 app.get('/', (_, res) => {
-    res.render('index')
+    res.render('index', {persons})
 })
 
 app.post('/add', (req, res) => {
-    console.log(req)
-    res.render('index')
+    console.log(req.body.person);
+    persons.push({
+        name: req.body.person,
+        email: req.body.personemail,
+        betreff: req.body.betreff,
+        inhalt: req.body.inhalt
+    })
+    console.log(persons)
+    res.render('index', {persons})
+
 })
 app.listen(PORT, () => {
     console.log('der Serverläuft on Port', PORT);
